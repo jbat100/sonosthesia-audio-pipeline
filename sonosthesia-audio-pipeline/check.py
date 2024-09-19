a95bf8679cabce42fa52c807c50357795cfd0d50
jbat100 <johnny.frenchy@gmail.com>
2024-09-19 13:53:35 +0200

    Ran validations

--- sonosthesia-audio-pipeline/check.py
+++ sonosthesia-audio-pipeline/check.py
@@ -1,45 +1,56 @@
 import argparse
 import os.path
+import json
+
+from jsonschema import validate
 
 from colorama import just_fix_windows_console
+from termcolor import colored
 
-from utils import (JSON_ANALYSIS_EXTENSION, input_to_filepaths, read_json_with_header, read_packed_with_header)
+from utils import (JSON_ANALYSIS_EXTENSION, MSGPACK_ANALYSIS_EXTENSION,
+                   input_to_filepaths, read_packed_with_header)
 
-CONVERSION_DESCRIPTION = "Convert between json and msgpack analysis files"
+CHECK_DESCRIPTION = "Check an analysis file against a schema"
 
 just_fix_windows_console()
 
 
-def run_conversion(file_path):
-    file_path_without_extension, extension = os.path.splitext(file_path)
+def run_check(file_path, schema):
+    print(colored(f'Running check against {file_path}', "blue"))
+    _, extension = os.path.splitext(file_path)
     if extension == JSON_ANALYSIS_EXTENSION:
-        header, data = read_json_with_header(file_path)
-        converted_file_path = change_extension(file_path, MSGPACK_ANALYSIS_EXTENSION)
-        write_packed_with_header(data, header, converted_file_path)
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            validate(data, schema)
     elif extension == MSGPACK_ANALYSIS_EXTENSION:
         header, data = read_packed_with_header(file_path)
-        converted_file_path = change_extension(file_path, JSON_ANALYSIS_EXTENSION)
-        write_json_with_header(data, header, converted_file_path)
+        validate(data, schema)
+    print(f'Passed {file_path}')
 
 
-def configure_conversion_parser(parser):
+def configure_check_parser(parser):
     parser.add_argument('-i', '--input', type=str, nargs='?', required=True,
                         help=f'path to file ({MSGPACK_ANALYSIS_EXTENSION} or {JSON_ANALYSIS_EXTENSION}) or directory')
+    parser.add_argument('-s', '--schema', type=str, required=True,
+                        help=f'path to a json schema file')
 
 
-def conversion_with_args(args):
+def check_with_args(args):
+    with open(args.schema, 'r') as file:
+        schema = json.load(file)
+    print(colored(f'Loaded schema from {args.schema}', "green"))
     file_paths = input_to_filepaths(args.input, [MSGPACK_ANALYSIS_EXTENSION, JSON_ANALYSIS_EXTENSION])
     for file_path in file_paths:
-        run_conversion(file_path)
+        run_check(file_path, schema)
     print('Done')
 
 
-def conversion():
-    parser = argparse.ArgumentParser(description=CONVERSION_DESCRIPTION)
-    configure_conversion_parser(parser)
+def check():
+    parser = argparse.ArgumentParser(description=CHECK_DESCRIPTION)
+    configure_check_parser(parser)
     args = parser.parse_args()
-    conversion_with_args(args)
+    check_with_args(args)
 
 
 if __name__ == "__main__":
-    conversion()
+    check()
